661dc7cc02c237dfbcc88d9d9433c53d81cf6af1
Connor Fricke <cd.fricke23@gmail.com>
2025-02-20 11:30:22 -0500

    monteCarlo integration added and tested. lots of functions pulled from rejection_sampling_v6.py as well

--- cpp/rejection_sampling.cpp
+++ cpp/rejection_sampling.cpp
@@ -27,59 +27,109 @@
 #include <thread>
 #include <cmath>
 #include <vector>
-#include "Integrate.h"
+#include <string>
+#include "Calculus.h"
 
-/*
-This function is the cumulative fractional area covered by
-rocks with diameter > D per area
-Basically says look at the ground, every square area Fk(D) of the
-area is covered by rocks that are at least D in diameter
-This is the distribution reported in Wu et al. (2021)
-*/
-// by Payton Linton
+/* define shapes */
+#define SPHERE 0
+#define TORUS 1
+#define CUBE 2
+#define PYRAMID 3
+#define CUBOID_A 4
+#define CUBOID_B 5
+/* define distributions */
+#define CE3 0
+#define CE4 1
+
+static const double PI = 4.0 * std::atan(1);    // PI
+static double GeoConst = 0.0;                   // value set by setGeoConst()
+
+/* FUNCTION PROTOTYPES */
+void setGeoConst(const int SHAPE);
+double Fk(const double D);
+double Fk_Di(const double D);
+double nDpm2(double D);
+double DnDpm2(double D);
+std::vector<double> NgtD4_Di(const double D, const double INF, const int N);
+
+double f(double x) {
+    return std::exp(-x);
+}
+
+int main() {
+
+    setGeoConst(SPHERE);
+
+    std::cout << calculus::integral::monteCarlo(f, 0., 1., 1000) << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
+
+
+/* FUNCTION DEFINITIONS */
+
+
+/* setGeoConst() */
+/* sets GeoConst global var. */
+void setGeoConst(const int SHAPE) {
+    if (SHAPE == SPHERE || SHAPE == PYRAMID) GeoConst = 4.0 / PI;
+    else if (SHAPE == CUBE || SHAPE == PYRAMID) GeoConst = 1.0;
+    else if (SHAPE == CUBOID_A) GeoConst = 1.0 / (0.8);
+    else if (SHAPE == CUBOID_B) GeoConst = 1.0 / (0.8 * 0.54);
+    else { 
+        std::cout << "Something went wrong! (Error 01)\n"; 
+        exit(1);
+    }
+}
+
+
+/* Fk() */
 double Fk(const double D) {
-    
     const double k = 0.0021;
     const double qk = 0.5648 + (0.01258 / k);
     return k * std::exp(-qk * D);
 }
 
+
+/* Fk_Di() */
 double Fk_Di(const double D) {
-    /*
-    This function is the cumulative fractional area covered by
-    rocks with diameter > D per area
-    Basically says look at the ground, every square area Fk(D) of the
-    area is covered by rocks that are at least D in diameter
-    This is the distribution reported in Wu et al. (2021)
-    */
     const double k = 0.0125;
     const double qk = 1.743;
     return k * std::exp(-qk * D);
 }
 
-std::vector<double> NgtD4_Di(const double D) {
+
+/* nDpm2() */
+double nDpm2(double D) {
+    const double k = 0.0135;
+    const double qk = 1.734;
+    return GeoConst * k * qk * std::exp(-qk * D) / (D * D);
+}
+
+
+/* DnDpm2() */
+double DnDpm2(double D) {
+    return D * nDpm2(D);
+}
+
+
+/* NgtD4_Di() */
+/* INF requires a numerical approximation for infinity. np.inf aint 'round here partner */
+/* ITER is the number of samples for the monteCarlo integration */
+std::vector<double> NgtD4_Di(const double D, const double INF, const int N) {
+
+    using calculus::integral::monteCarlo;
+
     std::vector<double> Diam_Array = {};
     for (double d = D; d < 3.0; d += 0.001) {
         Diam_Array.push_back(d);
     }
 
-    std::vector<double> N = {};
-    /*
+    std::vector<double> results = {};
     for (int i = 0; i < Diam_Array.size(); i++) {
-        double integral = quad(nDpm2, Diam_Array[i], std::inf )
-        N.push_back()
+        double integral = monteCarlo(nDpm2, Diam_Array[i], INF, N);
+        results.push_back(integral);
     }
-        */
-    return N;
-}
-
-double test(double x) {
-    return std::exp(x*x);
-}
-
-int main() {
-
-    std::cout << integrate::trapezoidal(test, 0.0, 1.0, 1000) << std::endl;
-
-    return EXIT_SUCCESS;
+    return results;
 }
