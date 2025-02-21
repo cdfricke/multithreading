7888c531ee6581c3a70f07ec12a3884797c63d6b
Connor Fricke <cd.fricke23@gmail.com>
2025-02-20 21:01:20 -0500

    translated all necessary functions

--- cpp/rejection_sampling.cpp
+++ cpp/rejection_sampling.cpp
@@ -38,11 +38,20 @@
 #define PYRAMID 3
 #define CUBOID_A 4
 #define CUBOID_B 5
+
 /* DEFINE DISTRIBUTIONS */
 #define CE3 0
 #define CE4 1
 
-/* GLOBAL VARS */
+struct Cuboid
+{
+    double x, y, z;
+};
+
+typedef std::vector<double> Vector;
+typedef std::vector<Cuboid> CuboidVector;
+
+/* DEFINE GLOBAL VARS */
 static const double PI = 4.0 * std::atan(1);    // PI
 static double GeoConst = 0.0;                   // controlled by individual funcs with setGeoConst()
 
@@ -51,19 +60,32 @@ void setGeoConst(int SHAPE);
 double Fk(double D);
 double Fk_Di(double D);
 double nDpm2(double D);
-double nDpm3(double D);
-double DnDpm3(double D);
+double nDpm3(double D, const int SHAPE, const int DIST);
+double DnDpm3(double D, const int SHAPE, const int DIST);
 double expintA(double x);
 double NgtD4(double D, const int SHAPE);
-std::vector<double> NgtD4_Di(double D, const double INF, const int N);
+double hpD(double x);
+double bpD(double x);
 
-double f(double x) {
-    return std::exp(-x);
-}
+Vector NgtD4_Di(double D, const double INF, const int N);
+Vector N_SVII(double Dmin, double Dmax);   // TODO: Do we want/need these implemented??
+Vector F_SVII(double Dmin, double Dmax);   // ??
+Vector n_a_SVII(double D);                 // ?? also is D a list or a single val
+Vector n_V_SVII(double D);                 // ??
 
+// TODO: Does this function really NEED to return a list of D?
+// Just create D on the spot, it's arange()?
+Vector CheckNgtD(Vector& rocks, double Dmin, double Dmax, double Dstep, double A);
+Vector CheckNgtD(CuboidVector& rocks, double Dmin, double Dmax, double Dstep, double A);
+
+Vector CheckFgtD(Vector& rocks, double Dmin, double Dmax, double Dstep, double A, const int SHAPE);
+Vector CheckFgtD(CuboidVector& rocks, double Dmin, double Dmax, double Dstep, double A);
+
+
+/* MAIN PROGRAM */
 int main() {
 
-    std::cout << calculus::integral::monteCarlo(f, 0., 1., 1000) << std::endl;
+    
 
     return EXIT_SUCCESS;
 }
@@ -96,15 +118,15 @@ double Fk(double D) {
 
 /* Fk_Di() */
 double Fk_Di(double D) {
-    double k = 0.0125;
-    double qk = 1.743;
+    double k = 0.0135;
+    double qk = 1.734;
     return k * std::exp(-qk * D);
 }
 
 
 /* nDpm2() */
-double nDpm2(double D, const int SHAPE) {
-    setGeoConst(SHAPE);
+/* We integrate this function, so GeoConst needs to be set externally! */
+double nDpm2(double D) {
     double k = 0.0135;
     double qk = 1.734;
     return GeoConst * k * qk * std::exp(-qk * D) / (D * D);
@@ -116,8 +138,8 @@ double nDpm3(double D, const int SHAPE, const int DIST) {
     setGeoConst(SHAPE);
     if (DIST == CE3)
     {
-        double k = 0.0125;
-        double qk = 1.743;
+        double k = 0.0135;
+        double qk = 1.734;
         return GeoConst * k * qk * std::exp(-qk * D) / (D*D*D);
     }
     else if (DIST == CE4)
@@ -129,36 +151,47 @@ double nDpm3(double D, const int SHAPE, const int DIST) {
 }
 
 /* DnDpm3() */
-double DnDpm3(double D, const int SHAPE, const int DIST)
-{
+double DnDpm3(double D, const int SHAPE, const int DIST) {
     return D * nDpm3(D, SHAPE, DIST);
 }
 
 
 /* expintA() */
-double expintA(double x)
-{
+double expintA(double x) {
     double A = std::log((0.56146 / x + 0.65) * (1.0 + x));
     double B = (x*x*x*x) * std::exp(7.7 * x) * std::pow((2.0 + x), 3.7);
     return std::pow((std::pow(A, -7.7) + B), -0.13);
 }
 
 /* NgtD4() */
-double NgtD4(double D, const int SHAPE)
-{    
+double NgtD4(double D, const int SHAPE) {    
     setGeoConst(SHAPE);
     double x = 6.555 * D;
     return GeoConst * 0.0137608 * (std::exp(-x) / D - 6.555 * expintA(x));
 }
 
+double hpD(double x) {
+    double mean = 0.54;
+    double std = 0.03;
+    double expo = 0.5 * ((x - mean) / std) * ((x - mean) / std);
+    return std::exp(-expo);
+}
+
+double bpD(double x) {
+    double mean = 0.8;
+    double std = 0.16;
+    double expo = 0.5 * ((x - mean) / std) * ((x - mean) / std);
+    return std::exp(-expo);
+}
+
 
 /* NgtD4_Di() */
 /* INF requires a numerical approximation for infinity. np.inf aint 'round here partner */
-/* ITER is the number of samples for the monteCarlo integration */
+/* N is the number of samples for the integration. Larger INF requires more samples, so choose wisely */
 std::vector<double> NgtD4_Di(double D, const double INF, const int N) {
 
     using calculus::integral::monteCarlo;
-
+    setGeoConst(SPHERE); // TODO: CHECK WHETHER THIS IS THE RIGHT MOVE HERE
     // combined two for loops into one from Rejection_Sampling_V6.py
     std::vector<double> results = {};
     for (double d = D; d < 3.0; d += 0.001) {
@@ -167,4 +200,72 @@ std::vector<double> NgtD4_Di(double D, const double INF, const int N) {
     }
 
     return results;
+}
+
+/* CheckNgtD() */
+/* NON-CUBOID VERSION */
+Vector CheckNgtD(Vector& rocks, double Dmin, double Dmax, double Dstep, double A)
+{
+    Vector N_rocks = {};
+    // first off, just iterate based on params
+    for (double d = Dmin; d < Dmax; d += Dstep)
+    {
+        int cuml = 0;
+        // "rock" is just another diameter
+        for (double rock : rocks) {
+            if (rock >= d) cuml++;
+        }
+        N_rocks.push_back(cuml / A);
+    }
+    return N_rocks;
+}
+/* CUBOID VERSION */
+Vector CheckNgtD(CuboidVector& rocks, double Dmin, double Dmax, double Dstep, double A)
+{
+    Vector N_rocks = {};
+    // first off, just iterate based on params
+    for (double d = Dmin; d < Dmax; d += Dstep) {
+        int cuml = 0;
+        // "rock" is now a Cuboid, with x, y, and z values
+        for (Cuboid rock : rocks) {
+            if (rock.x >= d) cuml++;
+        }
+        N_rocks.push_back(cuml / A);
+    }
+    return N_rocks;
+}
+
+/* CheckFgtD() */
+/* NON-CUBOID VERSION -> this one requires specifying the shape */
+Vector CheckFgtD(Vector& rocks, double Dmin, double Dmax, double Dstep, double A, const int SHAPE)
+{
+    Vector F_area = {};
+    for (double d = Dmin; d < Dmax; d += Dstep) {
+        double cuml = 0.0;
+        // once again, "rock" is just a diameter
+        for (double rock : rocks) {
+            if (SHAPE == CUBE) cuml += rock * rock;
+            else if (SHAPE == SPHERE) cuml += (PI / 4.0) * rock * rock;
+            else {
+                std::cout << "Something went wrong! (Error 02)\n";
+                exit(2);
+            }
+        }
+        F_area.push_back(cuml / A);
+    }
+    return F_area;
+}
+/* CUBOID VERSION */
+Vector CheckFgtD(CuboidVector& rocks, double Dmin, double Dmax, double Dstep, double A)
+{
+    Vector F_area = {};
+    for (double d = Dmin; d < Dmax; d += Dstep) {
+        double cuml = 0.0;
+        // "rock" is now a Cuboid
+        for (Cuboid rock : rocks) {
+            cuml += rock.x * rock.y;
+        }
+        F_area.push_back(cuml / A);
+    }
+    return F_area;
 }
