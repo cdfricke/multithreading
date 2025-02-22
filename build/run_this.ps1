3822ab9fc5f58635e7da4f84dde3ba52cf6526e8
Connor Fricke <cd.fricke23@gmail.com>
2025-02-21 22:35:20 -0500

    default params, args optional

--- build/run_this.ps1
+++ build/run_this.ps1
@@ -3,8 +3,8 @@
 # Build the project with this script from the project directory with 
 #    powershell -File .\build\run_this.ps1
 
-g++.exe -g .\cpp\rejection_sampling.cpp .\cpp-library\Calculus.cpp -o .\build\rejection_sampling.exe -I .\cpp-library
+g++.exe -Wall -Werror -g .\cpp\rejection_sampling.cpp .\cpp-library\Calculus.cpp -o .\build\rejection_sampling.exe -I .\cpp-library
 
 # to add arguments to the program, append $arguments to the end of the executable
-$arguments = 1, 0.05, 3.0, 25000    # SHAPE, DMIN, DMAX, VOL
-.\build\rejection_sampling.exe $arguments
+$arguments = 0, 0, 0.05, 3.0, 10.0, 2500.0    # SHAPE, DIST, DMIN, DMAX, DEPTH, AREA
+.\build\rejection_sampling.exe
