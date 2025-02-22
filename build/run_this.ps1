88d56d9456445e31fd815684505cb2b75ed5088f
Connor Fricke <cd.fricke23@gmail.com>
2025-02-21 19:55:26 -0500

    update to reflect new parameters

--- build/run_this.ps1
+++ build/run_this.ps1
@@ -6,5 +6,5 @@
 g++.exe -g .\cpp\rejection_sampling.cpp .\cpp-library\Calculus.cpp -o .\build\rejection_sampling.exe -I .\cpp-library
 
 # to add arguments to the program, append $arguments to the end of the executable
-$arguments = 1, 0.05, 100000    # SHAPE, DMIN, VOL
+$arguments = 1, 0.05, 3.0, 25000    # SHAPE, DMIN, DMAX, VOL
 .\build\rejection_sampling.exe $arguments
