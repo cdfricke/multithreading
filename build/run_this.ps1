72de5fec1b041f33690775a25c64e7c94af32479
Connor Fricke <cd.fricke23@gmail.com>
2025-02-23 21:39:11 -0500

    update run_this.ps1, added comments

--- build/run_this.ps1
+++ build/run_this.ps1
@@ -3,8 +3,28 @@
 # Build the project with this script from the project directory with 
 #    powershell -File .\build\run_this.ps1
 
-g++.exe -Wall -Werror -g .\cpp\rejection_sampling.cpp .\cpp-library\Calculus.cpp -o .\build\rejection_sampling.exe -I .\cpp-library
+# * COMPILING *
+# compile with g++, show all warnings and elevate all warnings to errors
+# add my library to include path, and add the .cpp files for that library to the compilation files list
+# the -D option specifies a define, by removing it, the program won't write data to any files
+$cppFiles = "cpp/rejection_sampling.cpp", "cpp-library/Calculus.cpp", "cpp-library/Stats.cpp"
+$includeDir = "cpp-library"
+$executable = ".\build\rejection_sampling.exe"
+g++.exe -Wall -Werror -O2 `
+-I $includeDir -D WRITE_DATA `
+$cppFiles `
+-o $executable
 
-# to add arguments to the program, append $arguments to the end of the executable
-$arguments = 0, 0, 0.05, 3.0, 10.0, 2500.0    # SHAPE, DIST, DMIN, DMAX, DEPTH, AREA
-.\build\rejection_sampling.exe
+# * VARS *
+$SPHERE = 0
+#$CUBE = 1
+#$CUBOID_A = 2
+#$CUBOID_B = 3
+$CE3 = 0
+#$CE4 = 1
+
+# * RUN PROGRAM *
+# to use custom arguments to the program, append $arguments to the end of the executable
+# and likewise remove it to use default parameters
+$arguments = $SPHERE, $CE3, 0.05, 3.0, 10.0, 2500.0    # SHAPE, DIST, DMIN, DMAX, DEPTH, AREA
+.\build\rejection_sampling.exe $arguments
