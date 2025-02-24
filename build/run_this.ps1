4e71a4eabec3c2075b1f4f11dbe2a29dac4901d5
Connor Fricke <cd.fricke23@gmail.com>
2025-02-24 14:09:53 -0500

    update build script

--- build/run_this.ps1
+++ build/run_this.ps1
@@ -7,24 +7,25 @@
 # compile with g++, show all warnings and elevate all warnings to errors
 # add my library to include path, and add the .cpp files for that library to the compilation files list
 # the -D option specifies a define, by removing it, the program won't write data to any files
-$cppFiles = "cpp/rejection_sampling.cpp", "cpp-library/Calculus.cpp", "cpp-library/Stats.cpp"
+$cppFiles = "cpp\rejection_sampling.cpp", "cpp-library\Calculus.cpp", "cpp-library\Stats.cpp"
 $includeDir = "cpp-library"
 $executable = ".\build\rejection_sampling.exe"
-g++.exe -Wall -Werror -O2 `
+g++.exe -Wall -Werror -O3 `
 -I $includeDir -D WRITE_DATA `
 $cppFiles `
 -o $executable
 
 # * VARS *
 $SPHERE = 0
-#$CUBE = 1
-#$CUBOID_A = 2
-#$CUBOID_B = 3
+$CUBE = 1
+$CUBOID_A = 2
+$CUBOID_B = 3
 $CE3 = 0
-#$CE4 = 1
+$CE4 = 1
 
 # * RUN PROGRAM *
 # to use custom arguments to the program, append $arguments to the end of the executable
 # and likewise remove it to use default parameters
-$arguments = $SPHERE, $CE3, 0.05, 3.0, 10.0, 2500.0    # SHAPE, DIST, DMIN, DMAX, DEPTH, AREA
+# arguments list: SHAPE, DIST, DMIN, DMAX, DSTEP, DEPTH, AREA
+$arguments = $SPHERE, $CE3, 0.05, 3.0, 0.001, 10.0, 2500.0    
 .\build\rejection_sampling.exe $arguments
