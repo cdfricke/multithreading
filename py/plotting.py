5ffc78545fefd608b210de69922f0813e386e3e3
Connor Fricke <cd.fricke23@gmail.com>
2025-02-24 14:16:54 -0500

    added plotting.py

---
+++ py/plotting.py
@@ -0,0 +1,8 @@
+# PLOTTING SCRIPT FOR rejection_sampling.cpp
+# most of this comes from Rejection_Sampling_V6.py by Payton Linton (linton.93@osu.edu)
+# Adapted for work with multithreading program by Connor Fricke (fricke.59@osu.edu)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
