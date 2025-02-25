c0347c25e7adfb55a38088632eb0d280253c39a0
Connor Fricke <cd.fricke23@gmail.com>
2025-02-24 23:14:14 -0500

    plots still not coming out right

--- py/plotting.py
+++ py/plotting.py
@@ -1,12 +1,19 @@
 # PLOTTING SCRIPT FOR rejection_sampling.cpp
 # most of this comes from Rejection_Sampling_V6.py by Payton Linton (linton.93@osu.edu)
 # Adapted for work with multithreading program by Connor Fricke (fricke.59@osu.edu)
+# Latest Rev: 24-Feb-2025
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
 
+# * GLOBAL CONSTS *
+k = 0.0
+qk = 0.0
+GeoConst = 0.0
+
+# * PROGRAM PARAMS *
 SHAPE = int(sys.argv[1])
 DIST = int(sys.argv[2])
 DMIN = float(sys.argv[3])
@@ -15,62 +22,103 @@ DSTEP = float(sys.argv[5])
 DEPTH = float(sys.argv[6])
 AREA = float(sys.argv[7])
 
+# * INTERPOLATE GLOBAL VARS FROM PARAMS *
 shapeStr = ""
+distStr = ""
 if SHAPE == 0:
     shapeStr = "SPHERE"
+    GeoConst = 4.0 / np.pi
 elif SHAPE == 1:
     shapeStr = "CUBE"
+    GeoConst = 1.0
 elif SHAPE == 2:
     shapeStr = "CUBOID_A"
+    GeoConst = 1.0 / (0.8)
 elif SHAPE == 3:
     shapeStr = "CUBOID_B"
+    GeoConst = 1.0 / (0.8*0.54)
 else:
-    print("Something went wrong!")
-    exit()
+    print("Something went wrong! (Shape Invalid)")
+    exit() 
 
+if (DIST == 0): 
+    distStr = "CE3"
+    k = 0.0125
+    qk = 1.743
+elif (DIST == 1):
+    distStr = "CE4"
+    k = 0.0021
+    qk = 0.5648 + 0.01258 / k
+else:
+    print("Something went wrong! (Distribution Invalid)")
+    exit   
+
+# * FUNCTIONS WE STILL NEED *
+def nDpm2(D):
+    return GeoConst*k*qk*np.exp(-qk*D)/D**2
+def NgtD4_Di(D):
+    from scipy.integrate import quad
+    Diam_array = Sampled_D
+    N = []
+    for i in range(len(Diam_array)):
+        N.append(quad(nDpm2, Diam_array[i], np.inf)[0])
+    return N
+def Fk_Di(D):
+    return k*np.exp(-qk*D)
+
+# * GENERATE ARRAYS AND DF *
 rockDataFileName = './data/Rock_Data_' + str(int(AREA)) + 'XY_' + str(int(DEPTH)) + 'Z_' + shapeStr + '.csv'
-df = pd.read_csv(rockDataFileName, delimiter=',')
-print(df.head())
+df_RockData = pd.read_csv(rockDataFileName, delimiter=',')
+Avg_Sampled_NgtD = np.loadtxt('./data/Avg_Sampled_NgtD.csv', delimiter=',')
+Avg_Sampled_FgtD = np.loadtxt('./data/Avg_Sampled_FgtD.csv', delimiter=',')
+Sampled_D = np.arange(DMIN, DMAX, DSTEP)
 
-# df.to_csv('./Rock_Data_' + str(AREA) + 'XY_' + str(DEPTH) + 'Z_' + shapeStr + '.csv')
+# if C++ generated arrays are larger or smaller than np.arange(DMIN, DMAX, DSTEP), then truncate to match lengths
+while (len(Avg_Sampled_NgtD) > len(Sampled_D)):
+    Avg_Sampled_NgtD = Avg_Sampled_NgtD[:-1]
+while (len(Avg_Sampled_FgtD) > len(Sampled_D)):
+    Avg_Sampled_FgtD = Avg_Sampled_FgtD[:-1]
+while (len(Avg_Sampled_NgtD) < len(Sampled_D)):
+    Sampled_D = Sampled_D[:-1]
+while (len(Avg_Sampled_FgtD) < len(Sampled_D)):
+    Sampled_D = Sampled_D[:-1]
 
-# fig, axs = plt.subplots(2,2)
-# axs[0,0].plot(Sampled_D, NgtD4_Di(D), label = 'Wu et al. (2021)')
-# for i in range(len(Avg_Sampled_NgtD)):
-#     axs[0,0].plot(Sampled_D, Avg_Sampled_NgtD[i], label = "Rejection Sampling using " + str(Dist))
-# axs[0,0].legend()
-# axs[0,0].set_xlabel('D [m]')
-# axs[0,0].set_ylabel('$Rocks/m^2$')
-# axs[0,0].set_title("Number of rocks/$m^2$ with diameter > D, Area = " + str(Area) +', Depth = ' + str(Depth) + ': ' + Shape)
+print(Sampled_D[0:10])
+print(Avg_Sampled_NgtD[0:10])
+print(Avg_Sampled_FgtD[0:10])
 
-# axs[0,1].plot(np.arange(), Fk_Di(Sampled_D), label = 'Wu et al. (2021)')
-# for i in range(len(Avg_Sampled_FgtD)):
-#     axs[0,1].plot(Sampled_D, Avg_Sampled_FgtD[i], label = "Rejection Sampling using " + str(Dist))
-# axs[0,1].legend()
-# axs[0,1].set_xlabel('D [m]')
-# axs[0,1].set_ylabel('Fractional Area')
-# axs[0,1].set_title("Fractional area covered by rocks with diameter > D Area = " + str(Area) +', Depth = ' + str(Depth) + ': ' + Shape)
+# * PLOT *
+fig, axs = plt.subplots(2,2)
+axs[0,0].plot(Sampled_D, NgtD4_Di(DMIN), label = 'Wu et al. (2021)')
+axs[0,0].plot(Sampled_D, Avg_Sampled_NgtD, label = "Rejection Sampling using " + distStr)
+axs[0,0].legend()
+axs[0,0].set_xlabel('D [m]')
+axs[0,0].set_ylabel('$Rocks/m^2$')
+axs[0,0].set_title("Number of rocks/$m^2$ with diameter > D, Area = " + str(AREA) +', Depth = ' + str(DEPTH) + ': ' + shapeStr)
 
+axs[0,1].plot(Sampled_D, Fk_Di(Sampled_D), label = 'Wu et al. (2021)')
+axs[0,1].plot(Sampled_D, Avg_Sampled_FgtD, label = "Rejection Sampling using " + distStr)
+axs[0,1].legend()
+axs[0,1].set_xlabel('D [m]')
+axs[0,1].set_ylabel('Fractional Area')
+axs[0,1].set_title("Fractional area covered by rocks with diameter > D Area = " + str(AREA) +', Depth = ' + str(DEPTH) + ': ' + shapeStr)
 
+axs[1,0].plot(Sampled_D, NgtD4_Di(DMIN), label = 'Wu et al. (2021)')
+axs[1,0].plot(Sampled_D, Avg_Sampled_NgtD, label = "Rejection Sampling using " + distStr)
+axs[1,0].set_yscale('log')
+axs[1,0].set_xscale('log')
+axs[1,0].legend()
+axs[1,0].set_xlabel('D [m]')
+axs[1,0].set_ylabel('$Rocks/m^2$')
+axs[1,0].set_title("Number of rocks/$m^2$ with diameter > D Area = " + str(AREA) +', Depth = ' + str(DEPTH) + ': ' + shapeStr)
 
-# axs[1,0].plot(Sampled_D, NgtD4_Di(D), label = 'Wu et al. (2021)')
-# for i in range(len(Avg_Sampled_NgtD)):
-#     axs[1,0].plot(Sampled_D, Avg_Sampled_NgtD[i], label = "Rejection Sampling using " + str(Dist))
-# axs[1,0].set_yscale('log')
-# axs[1,0].set_xscale('log')
-# axs[1,0].legend()
-# axs[1,0].set_xlabel('D [m]')
-# axs[1,0].set_ylabel('$Rocks/m^2$')
-# axs[1,0].set_title("Number of rocks/$m^2$ with diameter > D Area = " + str(Area) +', Depth = ' + str(Depth) + ': ' + Shape)
-
-# axs[1,1].plot(Sampled_D, Fk_Di(Sampled_D), label = 'Wu et al. (2021)')
-# for i in range(len(Avg_Sampled_FgtD)):
-#     axs[1,1].plot(Sampled_D, Avg_Sampled_FgtD[i], label = "Rejection Sampling using " + str(Dist))
-# axs[1,1].set_yscale('log')
-# axs[1,1].set_xscale('log')
-# axs[1,1].legend()
-# axs[1,1].set_xlabel('D [m]')
-# axs[1,1].set_ylabel('Fractional Area')
-# axs[1,1].set_title("Fractional area covered by rocks with diameter > D Area = " + str(Area) +', Depth = ' + str(Depth) + ': ' + Shape)
-# plt.tight_layout()
-# plt.show()
+axs[1,1].plot(Sampled_D, Fk_Di(Sampled_D), label = 'Wu et al. (2021)')
+axs[1,1].plot(Sampled_D, Avg_Sampled_FgtD, label = "Rejection Sampling using " + distStr)
+axs[1,1].set_yscale('log')
+axs[1,1].set_xscale('log')
+axs[1,1].legend()
+axs[1,1].set_xlabel('D [m]')
+axs[1,1].set_ylabel('Fractional Area')
+axs[1,1].set_title("Fractional area covered by rocks with diameter > D Area = " + str(AREA) +', Depth = ' + str(DEPTH) + ': ' + shapeStr)
+plt.tight_layout()
+plt.show()
